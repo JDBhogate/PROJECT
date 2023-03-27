a022949029d976b3829a9acb2bb425e63aab3f79
JDBhogate <129050974+JDBhogate@users.noreply.github.com>
2023-03-27 15:19:01 +0530

    ARRAY

---
+++ ArrayExample1.java
@@ -0,0 +1,18 @@
+package day6;
+
+public class ArrayExample1 {
+
+	public static void main(String[] args) {
+
+
+		 int p[]= new int[10];
+		
+		 p[0]=20;
+		 p[1]=223;
+		 p[2]=455;
+		 System.out.println(p[0] + " "+ p[1]+" "+ p[2]);
+		 
+
+	}
+
+}
